05e1279089461f184ae1ba0fc763768377a3509a
ELize6297 <100554690+ELize6297@users.noreply.github.com>
2022-09-22 18:32:17 +0800

    Add files via upload

---
+++ meal-planner/meal-planer.script.js
@@ -0,0 +1,24 @@
+// const btnRecipe = document.querySelectorAll(".btn-recipe");
+// const btnIngredients = document.querySelector(".btn-ingredients");
+// const containerRecipe = document.querySelector(".container-recipe");
+// const containerIngredients = document.querySelector(".container-ingredients");
+// let container;
+
+// const reveal = function (e) {
+//   e.preventDefault();
+//   container = e.target;
+//   containerRecipe.classList.remove("hidden");
+//   containerIngredients.classList.add("hidden");
+// };
+
+// btnRecipe.forEach((btn) => {
+//   // console.log(btn);
+//   return btn.addEventListener("click", reveal);
+//   // return btn.addEventListener("click", flipCard);
+// });
+
+// btnIngredients.addEventListener("click", function () {
+//   console.log("test");
+//   containerRecipe.classList.add("hidden");
+//   containerIngredients.classList.remove("hidden");
+// });
